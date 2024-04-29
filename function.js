8deca16a6b9efc9f49024297856798015c7cc62b
Antonino <drakon9927@libero.it>
2024-04-29 14:32:23 +0200

    Palindroma updates

--- function.js
+++ function.js
@@ -1,14 +1,19 @@
-function sePalindroma(parola) {
+// Capire se una parola è palindroma o no
+
+function sePalindroma(parolaUtente) {
     let left = 0;
-    let right = parola.length - 1;
+    let right = parolaUtente.length - 1;
 
     while (left < right) {
-        if (parola[left] !== parola[right]) {
+        if (parolaUtente[left] !== parolaUtente[right]) {
             return false; 
         }
         left++;  
         right--; 
     }
-
     return true; 
 }
+
+let risultato = sePalindroma(parolaUtente);
+console.log(risultato ? "La parola è palindroma." : "La parola non è palindroma.");
+
